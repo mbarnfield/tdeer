6848ba01f0881c92a24b9143414030f5b52f6ba3
Matthew Barnfield <m.barnfield@qmul.ac.uk>
2019-05-06 18:04:07 +0100

    adjusted tdee over weight change

--- R/tdee.R
+++ R/tdee.R
@@ -1,13 +1,14 @@
 #' @rdname tdee
 #' @title A TDEE calculator for R.
-#' @description Calculate your basal metabolic rate (BMR), total daily energy expenditure (TDEE), and calorie goals to lose, maintain, or gain weight.
+#' @description Calculate your basal metabolic rate (BMR), total daily energy expenditure (TDEE), and calorie goals to lose, maintain, or gain weight. Also calculate adjusted values for every kg interval between your current and goal weight.
 #' @param height Your height in centimetres. Returns a warning message to check unit of measurement if over 250 or under 100.
 #' @param weight Your weight in kilograms. Returns a warning message to check units of measurement if over 250 or under 30.
 #' @param age Your age in years. Returns a warning message to check units of measurement if over 100.
 #' @param sex Your biological sex. Throws an error if not 'male' or 'female'.
 #' @param activity Your activity level. Are you 'sedentary', or 'lightly', 'moderately', 'very' or 'extremely' active? Throws an error if not one of these options (do not include the word 'active', just the adverbs).
 #' @param aim Your weight aim. Do you want to 'lose', 'maintain', or 'gain' weight? Throws an error if not one of these options
-#' @return A dataframe with columns bmr, tdee, and calories
+#' @param goal An optional argument specifying a goal weight. If specified, `tdee` runs calculations at each weight between your current and goal weight to show how calorie consumption needs to be adapted over time.
+#' @return A dataframe with columns weight, bmr, tdee, and calories
 #' @details \code{tdee} calculates your BMR, TDEE, and calorie requirements based on your height, weight, age, sex, activity levels and weight aims. The calculations are based on this post: https://steelfitusa.com/2018/10/calculate-tdee/
 #' @import tidyverse
 #' @export
@@ -18,9 +19,10 @@
 #'      age = 23,
 #'      sex = "male",
 #'      activity = "moderately",
-#'      aim = "gain")
+#'      aim = "gain",
+#'      goal = "89")
 #' # alternatively without arg names
-#' tdee(182, 84, 23, "male", "moderately", "gain")
+#' tdee(182, 84, 23, "male", "moderately", "gain", 89)
 #' # a 150 cm, thirty-year-old woman weighing ninety kilograms, who is
 #' # sedentary and wants to lose weight
 #' tdee(height = 150,
@@ -29,14 +31,20 @@
 #'      sex = "female",
 #'      activity = "sedentary",
 #'      aim = "lose")
-
+#' # plot adjustments towards goal weight
+#' tdee_track <- tdee(182, 84, 23, "male", "moderately", "gain", 89)
+#' ggplot(tdee_track, mapping = aes(x = weight, y = calories)) +
+#'   geom_line() +
+#'   geom_point()
+#'
 
 tdee <- function(height,
                  weight,
                  age,
                  sex,
                  activity,
-                 aim) {
+                 aim,
+                 goal = NULL) {
 
   if(height >= 250 | height <= 100) {
     warning("Make sure you have input your height in cm")
@@ -58,6 +66,8 @@ tdee <- function(height,
   if(aim != "gain" & aim != "maintain" & aim != "lose") {
     stop("Aim must be one of: 'gain', 'maintain' or 'lose'")
   }
+
+  if(is.null(goal)) {
   #calculate basal metabolic rate
   bmr <- dplyr::case_when(
     sex == "female" ~
@@ -86,5 +96,76 @@ tdee <- function(height,
   )
 
   #dataframe of results
-  data.frame(bmr, tdee, calories)
+  data.frame(weight, bmr, tdee, calories)
+  }
+
+  else {
+
+    # vector of weights from current to one away from goal
+    one_away_gain <- goal-1
+    one_away_lose <- goal+1
+
+    if(aim == "gain") {
+      kgs <- weight:one_away_gain
+    }
+
+    if(aim =="lose") {
+      kgs <- weight:one_away_lose
+    }
+
+    #calculate basal metabolic rate
+    bmrs <- dplyr::case_when(
+      sex == "female" ~
+        655 + 9.6*kgs + 1.8*height - 4.7*age,
+      sex == "male" ~
+        66 + 13.7*kgs + 5*height - 6.8*age,
+      TRUE ~ 0
+    )
+
+    #calculate total daily energy expenditure
+    tdees <- dplyr::case_when(
+      activity == "sedentary" ~ floor(bmrs*1.2),
+      activity == "lightly" ~ floor(bmrs*1.375),
+      activity == "moderately" ~ floor(bmrs*1.55),
+      activity == "very" ~ floor(bmrs*1.725),
+      activity == "extremely" ~ floor(bmrs*1.9),
+      TRUE ~ 0
+    )
+
+    #calculate calorie goals
+    calories <- dplyr::case_when(
+      aim == "gain" ~ tdees + 300,
+      aim == "lose" ~ floor(tdees*0.8),
+      TRUE ~ 0
+    )
+
+    #calculate maintenance at goal weight
+    bmr_goal <- dplyr::case_when(
+      sex == "female" ~
+        655 + 9.6*goal + 1.8*height - 4.7*age,
+      sex == "male" ~
+        66 + 13.7*goal + 5*height - 6.8*age,
+      TRUE ~ 0
+    )
+
+    tdee_goal <- dplyr::case_when(
+      activity == "sedentary" ~ floor(bmr_goal*1.2),
+      activity == "lightly" ~ floor(bmr_goal*1.375),
+      activity == "moderately" ~ floor(bmr_goal*1.55),
+      activity == "very" ~ floor(bmr_goal*1.725),
+      activity == "extremely" ~ floor(bmr_goal*1.9),
+      TRUE ~ 0
+    )
+
+    calories_goal <- tdee_goal
+
+    weight <- c(kgs, goal)
+    bmr <- c(bmrs, bmr_goal)
+    tdee <- c(tdees, tdee_goal)
+    calories <- c(calories, calories_goal)
+
+    #dataframe of results
+    data.frame(weight, bmr, tdee, calories)
+  }
 }
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tdeer
-`tdeer`, through its function [`tdee()`](https://github.com/mbarnfield/tdeer/blob/master/R/tdee.R), allows you to calculate your total daily energy expenditure in R. In doing so, it tells you how many calories to consume to attain your goal of gaining, losing, or maintaining weight. 
+`tdeer`, through its function [`tdee()`](https://github.com/mbarnfield/tdeer/blob/master/R/tdee.R), allows you to calculate your total daily energy expenditure in R. In doing so, it tells you how many calories to consume to attain your goal of gaining, losing, or maintaining weight. If you provide a goal weight, it will also run these same calculations for every kilogram interval between you current and goal weight.
 
 This package is pretty pointless, because there are loads of [online calculators](https://tdeecalculator.net) out there which do the job perfectly well within your browser. But if you're weird like me and want to do as much in RStudio as possible, including calculating your TDEE, `tdeer` is the package for you...
 
